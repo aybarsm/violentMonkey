a89dd62c118c03dd1dcfad361bf8f15fe37e4a5a
Murat Aybars <39916128+aybarsm@users.noreply.github.com>
2026-03-17 20:42:55 +0400

    add GM_config submodule

--- serve-dev.sh
+++ serve-dev.sh
@@ -5,6 +5,11 @@ set -euo pipefail
 PATH_SELF="$(realpath "${BASH_SOURCE[0]}")"
 PATH_DIR_SELF="$(dirname "${PATH_SELF}")"
 
-EXEC_ARGS=( "serve" "webdav" "${PATH_DIR_SELF}" "--addr" "127.0.0.1:8901" "--vfs-cache-mode" "full" )
+EXEC_ARGS=( 
+    "serve" "webdav" "${PATH_DIR_SELF}" "--addr" "127.0.0.1:8901" 
+    "--vfs-cache-mode" "off" "--dir-cache-time" "1s" "--vfs-read-chunk-size" "32M"
+    "--vfs-read-chunk-size-limit" "off"
+    "--log-level" "INFO"
+)
 
 exec rclone "${EXEC_ARGS[@]}"
--- src/scot_courts_civil.js
+++ src/scot_courts_civil.js
@@ -1,75 +1,47 @@
 /// <reference types="@violentmonkey/types" />
 // ==UserScript==
-// @name         Scot Civil Case File Sync
+// @name         Scot Courts - Civil Case File Sync
 // @namespace    https://github.com/aybarsm
-// @version      0.4
-// @description  Scot Civil Case File Sync
+// @version      0.1
+// @description  Scot Courts - Civil Case File Sync
 // @author       aybarsm
 // @match        https://civilonline.scotcourts.gov.uk/*
-// @require      https://cdn.jsdelivr.net/npm/bootstrap@5.3.8/dist/js/bootstrap.bundle.min.js
-// @resource     BOOTSTRAP_CSS https://cdn.jsdelivr.net/npm/bootstrap@5.3.8/dist/css/bootstrap.min.css
-// @grant        GM_log
-// @grant        GM_getValue
 // @grant        GM_setValue
+// @grant        GM_getValue
+// @grant        GM_deleteValue
+// @grant        GM_registerMenuCommand
+// @grant        GM_unregisterMenuCommand
+// @grant        GM_addValueChangeListener
+// @grant        GM_log
 // @grant        GM.xmlHttpRequest
 // @grant        GM_addStyle
 // @grant        GM_getResourceText
 // @grant        GM_notification
+// @require      https://github.com/PRO-2684/GM_config/releases/download/v1.2.2/config.min.js#md5=c45f9b0d19ba69bb2d44918746c4d7ae
 // ==/UserScript==
 
-function isValidJson(data) {
-    if (typeof data !== 'string') return false;
-
-    try {
-        JSON.parse(data);
-        return true;
-    } catch {
-        return false;
-    }
-}
-
-function isPlainObject(data) {
-    return data != null && typeof data === 'object' && data.constructor === Object && !Array.isArray(data);
-}
-
-function deepMergeImmutable(source, target) {
-    const output = structuredClone(target);
-
-    for (const key in source) {
-        if (!Object.prototype.hasOwnProperty.call(source, key)) continue;
-
-        const sourceValue = source[key];
-
-        if (isPlainObject(sourceValue) && isPlainObject(output[key])) {
-            output[key] = deepMergeImmutable(output[key], sourceValue);
-        } else {
-            output[key] = sourceValue;
-        }
-    }
-
-    return output;
-}
-
-const vmSettingsDefault = {
-    cases: {},
+const configSpec = {
+    price: {
+        name: "Price",
+        type: "int",
+        value: 100
+    },
+    name: {
+        name: "Name",
+        type: "str",
+        value: "John Doe"
+    },
 };
+
+const config = new GM_config(configSpec);
+
+config.addEventListener("set", (e) => {
+    const { prop, before, after } = e.detail;
+    console.log(`Config ${prop} is modified from ${before} to ${after}`);
+});
 
 (function() {
     'use strict';
-    
-    GM_config.init({
-        id: "ScotCourts_Civil",
-        title: "Scot Courts - Civil",
-        fields: {
-            amazon: {
-                type: "checkbox",
-                label: "Amazon",
-                title: "Uncheck if you don't use Amazon Prime Video",
-                section: "Main Settings",
-                default: true,
-            },
-        }
-    });
     
     // let vmCache = {
     //     'page': location.pathname.startsWith('/rooms/'),
